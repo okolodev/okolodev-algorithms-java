45d0fb3925d7701e05b81ae419feec0358ee1945
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2015-12-29 22:06:04 +0100

    Fixed tests for Dictionary for the 17th meetup.

--- src/main/java/meetup_17_dictionary/HashDictionary.java
+++ src/main/java/meetup_17_dictionary/HashDictionary.java
@@ -1,39 +1,149 @@
 package meetup_17_dictionary;
 
+import java.math.BigInteger;
+import java.util.Objects;
+
 public class HashDictionary<K, V> implements Dictionary<K, V> {
+    private final double loadFactor;
+
+    private int size;
+    private Entry<K, V>[] buckets;
+
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
+        private Entry<K, V> next;
+
+        public Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     public HashDictionary(double loadFactor) {
-        throw new UnsupportedOperationException("Implement me!");
+        this.loadFactor = loadFactor;
+        buckets = (Entry<K, V>[]) new Entry[1];
     }
 
     @Override
     public void insert(K key, V value) {
-        throw new UnsupportedOperationException("Implement me!");
+        Entry<K, V> entry = findEntry(key);
+
+        if (entry != null) {
+            entry.value = value;
+            return;
+        }
+
+        ensureLoading();
+        int hashIndex = hashIndex(key);
+        buckets[hashIndex] = new Entry<>(key, value, buckets[hashIndex]);
+        size++;
+    }
+
+    private void ensureLoading() {
+        int capacity = buckets.length;
+        double load = capacity * loadFactor;
+
+        if (size >= load) {
+            int newCapacity = nextPrime(capacity * 2);
+            buckets = rehash(newCapacity);
+        } else if (load / 2 >= size) {
+            int newCapacity = nextPrime(capacity / 2);
+            buckets = rehash(newCapacity);
+        }
+    }
+
+    private Entry<K, V>[] rehash(int newCapacity) {
+        Entry<K, V>[] newBuckets = (Entry<K, V>[]) new Entry[newCapacity];
+
+        for (Entry<K, V> entry : buckets) {
+            while (entry != null) {
+                int hashIndex = hashIndex(entry.key, newCapacity);
+                newBuckets[hashIndex] = new Entry<>(entry.key, entry.value,
+                        newBuckets[hashIndex]);
+                entry = entry.next;
+            }
+        }
+
+        return newBuckets;
+    }
+
+    private int nextPrime(int n) {
+        return BigInteger.valueOf(n).nextProbablePrime().intValue();
     }
 
     @Override
     public boolean contains(K key) {
-        throw new UnsupportedOperationException("Implement me!");
+        return findEntry(key) != null;
+    }
+
+    private int hashIndex(K key) {
+        return hashIndex(key, buckets.length);
+    }
+
+    private int hashIndex(K key, int capacity) {
+        return Math.abs(Objects.hashCode(key) % capacity);
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException("Implement me!");
+        Entry<K, V> entry = findEntry(key);
+
+        if (entry != null) {
+            return entry.value;
+        }
+
+        return null;
+    }
+
+    private Entry<K, V> findEntry(K key) {
+        int hashIndex = hashIndex(key);
+        Entry<K, V> entry = buckets[hashIndex];
+
+        while (entry != null) {
+            if (Objects.equals(key, entry.key)) {
+                return entry;
+            }
+            entry = entry.next;
+        }
+
+        return null;
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException("Implement me!");
+        int hashIndex = hashIndex(key);
+        Entry<K, V> entry = buckets[hashIndex];
+        Entry<K, V> previousEntry = null;
+
+        while (entry != null) {
+            if (key.equals(entry.key)) {
+                if (previousEntry == null) {
+                    buckets[hashIndex] = entry.next;
+                } else {
+                    previousEntry.next = entry.next;
+                }
+
+                size--;
+                return;
+            }
+
+            previousEntry = entry;
+            entry = entry.next;
+        }
+
+        ensureLoading();
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Implement me!");
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Implement me!");
+        return size == 0;
     }
 
 }
