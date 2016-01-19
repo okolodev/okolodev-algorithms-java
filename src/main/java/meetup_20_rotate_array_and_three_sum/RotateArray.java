07ec5f00258837b4e8020d9ecc256664dd28092c
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2016-01-19 21:51:06 +0100

    Fixed tests for Rotate Array and 3 Sum for the 20th meetup.

--- src/main/java/meetup_20_rotate_array_and_three_sum/RotateArray.java
+++ src/main/java/meetup_20_rotate_array_and_three_sum/RotateArray.java
@@ -1,5 +1,9 @@
 package meetup_20_rotate_array_and_three_sum;
 
+import meetup_06_stack_queue_dynamic_array.adt.DynamicList;
+import meetup_06_stack_queue_dynamic_array.adt.LinkedDynamicList;
+import stack1.adt.ListNode;
+
 /**
  * <p>
  * 189. Rotate Array
@@ -10,27 +14,160 @@ package meetup_20_rotate_array_and_three_sum;
 public class RotateArray {
 
     public void rotate(int[] numbers, int positions) {
-        throw new UnsupportedOperationException("Implement me!");
+        int length = numbers.length;
+
+        if (length <= 1) {
+            return;
+        }
+
+        positions = positions % length;
+
+        if (positions == 0) {
+            return;
+        }
+
+        reverse(numbers, 0, length - 1);
+        reverse(numbers, 0, positions - 1);
+        reverse(numbers, positions, length - 1);
+    }
+
+    private void reverse(int[] numbers, int low, int high) {
+        for (int i = low; i <= low + (high - low) / 2; i++) {
+            swap(numbers, i, high - (i - low));
+        }
+    }
+
+    private void swap(int[] numbers, int i, int j) {
+        int tmp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = tmp;
     }
 
     public void rotate2(int[] numbers, int positions) {
-        throw new UnsupportedOperationException("Implement me!");
+        int length = numbers.length;
+
+        if (length <= 1) {
+            return;
+        }
+
+        positions = positions % length;
+
+        if (positions == 0) {
+            return;
+        }
+
+        int[] doubleCopy = new int[2 * length];
+        System.arraycopy(numbers, 0, doubleCopy, 0, length);
+        System.arraycopy(numbers, 0, doubleCopy, length, length);
+
+        System.arraycopy(doubleCopy, length - positions, numbers, 0, length);
     }
 
     public void rotate3(int[] numbers, int positions) {
-        throw new UnsupportedOperationException("Implement me!");
+        int length = numbers.length;
+
+        if (length <= 1) {
+            return;
+        }
+
+        positions = positions % length;
+
+        if (positions == 0) {
+            return;
+        }
+
+        int[] copy = numbers.clone();
+
+        System.arraycopy(copy, 0, numbers, positions, length - positions);
+        System.arraycopy(copy, length - positions, numbers, 0, positions);
     }
 
     public void rotate4(int[] numbers, int positions) {
-        throw new UnsupportedOperationException("Implement me!");
+        int length = numbers.length;
+
+        if (length <= 1) {
+            return;
+        }
+
+        positions = positions % length;
+
+        if (positions == 0) {
+            return;
+        }
+
+        int[] copy = numbers.clone();
+
+        for (int i = 0; i < positions; i++) {
+            numbers[i] = copy[length - positions + i];
+        }
+
+        for (int i = positions; i < length; i++) {
+            numbers[i] = copy[i - positions];
+        }
     }
 
     public void rotate5(int[] numbers, int positions) {
-        throw new UnsupportedOperationException("Implement me!");
+        int length = numbers.length;
+
+        if (length <= 1) {
+            return;
+        }
+
+        positions = positions % length;
+
+        if (positions == 0) {
+            return;
+        }
+
+        DynamicList<Integer> list = new LinkedDynamicList<>();
+
+        for (int number : numbers) {
+            list.add(number);
+        }
+
+        for (int i = 0; i < length - positions; i++) {
+            int start = list.remove(0);
+            list.add(start);
+        }
+
+        for (int i = 0; i < length; i++) {
+            numbers[i] = list.get(i);
+        }
     }
 
     public void rotate6(int[] numbers, int positions) {
-        throw new UnsupportedOperationException("Implement me!");
+        int length = numbers.length;
+
+        if (length <= 1) {
+            return;
+        }
+
+        positions = positions % length;
+
+        if (positions == 0) {
+            return;
+        }
+
+        ListNode<Integer> start = new ListNode<>(numbers[0]);
+        ListNode<Integer> end = start;
+
+        for (int i = 1; i < length; i++) {
+            ListNode<Integer> next = new ListNode<>(numbers[i]);
+            end.setNext(next);
+            end = next;
+        }
+
+        ListNode<Integer> newStart = start;
+        for (int i = 0; i < length - positions; i++) {
+            newStart = newStart.getNext();
+        }
+
+        end.setNext(start);
+
+        for (int i = 0; i < length; i++) {
+            numbers[i] = newStart.getValue();
+            newStart = newStart.getNext();
+        }
     }
 
 }
--- src/main/java/meetup_20_rotate_array_and_three_sum/ThreeSum2.java
+++ src/main/java/meetup_20_rotate_array_and_three_sum/ThreeSum2.java
@@ -1,6 +1,8 @@
 package meetup_20_rotate_array_and_three_sum;
 
+import meetup_06_stack_queue_dynamic_array.adt.ArrayDynamicList;
 import meetup_06_stack_queue_dynamic_array.adt.DynamicList;
+import meetup_11_sorting.BottomUpMergeSort;
 
 /**
  * <p>
@@ -12,7 +14,46 @@ import meetup_06_stack_queue_dynamic_array.adt.DynamicList;
 public class ThreeSum2 {
 
     public DynamicList<DynamicList<Integer>> threeSum(Integer[] numbers) {
-        throw new UnsupportedOperationException("Implement me!");
+        DynamicList<DynamicList<Integer>> triplets = new ArrayDynamicList<>();
+        numbers = sort(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i > 0 && numbers[i].equals(numbers[i - 1])) {
+                continue;
+            }
+
+            int diff = 0 - numbers[i];
+            int low = i + 1;
+            int high = numbers.length - 1;
+
+            while (low < high) {
+                if (numbers[low] + numbers[high] == diff) {
+                    DynamicList<Integer> triplet =
+                            new ArrayDynamicList<>(numbers[i], numbers[low], numbers[high]);
+                    triplets.add(triplet);
+
+                    while (low < high && numbers[low].equals(numbers[low + 1])) {
+                        low++;
+                    }
+                    low++;
+
+                    while (low < high && numbers[high].equals(numbers[high - 1])) {
+                        high--;
+                    }
+                    high--;
+                } else if (numbers[low] + numbers[high] > diff) {
+                    high--;
+                } else {
+                    low++;
+                }
+            }
+        }
+
+        return triplets;
+    }
+
+    private Integer[] sort(Integer[] numbers) {
+        return new BottomUpMergeSort<Integer>().sort(numbers);
     }
 
 }
