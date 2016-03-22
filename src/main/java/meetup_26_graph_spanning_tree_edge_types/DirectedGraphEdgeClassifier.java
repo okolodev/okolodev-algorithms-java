f60d55aafe2c1a513270af22b0f504c537dc2341
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2016-03-22 21:53:56 +0100

    Graph Spanning Tree Edges Types for the 26th meetup.

--- src/main/java/meetup_26_graph_spanning_tree_edge_types/DirectedGraphEdgeClassifier.java
+++ src/main/java/meetup_26_graph_spanning_tree_edge_types/DirectedGraphEdgeClassifier.java
@@ -2,28 +2,89 @@ package meetup_26_graph_spanning_tree_edge_types;
 
 import meetup_22_graph_representation.DirectedGraph;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class DirectedGraphEdgeClassifier {
+    private final DirectedGraph digraph;
+    private boolean[] visited;
+    private final int[] startTime;
+    private final int[] finishTime;
+
+    private Collection<DirectedEdge> treeEdges = new ArrayList<>();
+    private Collection<DirectedEdge> backEdges = new ArrayList<>();
+    private Collection<DirectedEdge> forwardEdges = new ArrayList<>();
+    private Collection<DirectedEdge> crossEdges = new ArrayList<>();
 
     public DirectedGraphEdgeClassifier(DirectedGraph digraph) {
-        throw new UnsupportedOperationException("Implement me!");
+        this.digraph = digraph;
+        visited = new boolean[digraph.vertices()];
+        startTime = new int[digraph.vertices()];
+        finishTime = new int[digraph.vertices()];
+
+        dfs();
+    }
+
+    private void dfs() {
+        int time = 0;
+
+        for (int startVertex = 0; startVertex < digraph.vertices(); startVertex++) {
+            time = dfs(startVertex, time);
+        }
+    }
+
+    private int dfs(int vertex, int time) {
+        if (visited[vertex]) {
+            return time;
+        }
+
+        visited[vertex] = true;
+        startTime[vertex] = ++time;
+
+        for (int u : digraph.adjacentVerticesTo(vertex)) {
+            if (visited[u]) {
+                if (isBackEdge(vertex, u)) {
+                    backEdges.add(new DirectedEdge(vertex, u, EdgeType.BACK_EDGE));
+                } else if (isForwardEdge(vertex, u)) {
+                    forwardEdges.add(new DirectedEdge(vertex, u, EdgeType.FORWARD_EDGE));
+                } else {
+                    crossEdges.add(new DirectedEdge(vertex, u, EdgeType.CROSS_EDGE));
+                }
+            } else {
+                treeEdges.add(new DirectedEdge(vertex, u, EdgeType.TREE_EDGE));
+                time = dfs(u, time);
+            }
+        }
+
+        finishTime[vertex] = ++time;
+
+        return time;
+    }
+
+    private boolean isBackEdge(int v, int u) {
+        return startTime[v] > 0 && finishTime[u] == 0;
+    }
+
+    private boolean isForwardEdge(int v, int u) {
+        return startTime[v] < startTime[u] && finishTime[u] > 0;
     }
 
     public Collection<DirectedEdge> getTreeEdges() {
-        throw new UnsupportedOperationException("Implement me!");
+        return Collections.unmodifiableCollection(treeEdges);
     }
 
     public Collection<DirectedEdge> getBackEdges() {
-        throw new UnsupportedOperationException("Implement me!");
+        return Collections.unmodifiableCollection(backEdges);
     }
 
     public Collection<DirectedEdge> getForwardEdges() {
-        throw new UnsupportedOperationException("Implement me!");
+        return Collections.unmodifiableCollection(forwardEdges);
     }
 
     public Collection<DirectedEdge> getCrossEdges() {
-        throw new UnsupportedOperationException("Implement me!");
+        return Collections.unmodifiableCollection(crossEdges);
     }
 
 }
+
--- src/main/java/meetup_26_graph_spanning_tree_edge_types/UndirectedGraphEdgeClassifier.java
+++ src/main/java/meetup_26_graph_spanning_tree_edge_types/UndirectedGraphEdgeClassifier.java
@@ -2,20 +2,54 @@ package meetup_26_graph_spanning_tree_edge_types;
 
 import meetup_23_celebrity_problem_and_undirected_graph_representation.UndirectedGraph;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class UndirectedGraphEdgeClassifier {
+    private final UndirectedGraph graph;
+    private final boolean[] visited;
+    private final Collection<UndirectedEdge> treeEdges = new ArrayList<>();
+    private final Collection<UndirectedEdge> backEdges = new HashSet<>();
 
     public UndirectedGraphEdgeClassifier(UndirectedGraph graph) {
-        throw new UnsupportedOperationException("Implement me!");
+        this.graph = graph;
+        visited = new boolean[graph.vertices()];
+
+        dfs();
+    }
+
+    private void dfs() {
+        for (int startVertex = 0; startVertex < graph.vertices(); startVertex++) {
+            dfs(startVertex, startVertex);
+        }
+    }
+
+    private void dfs(int vertex, int parent) {
+        if (visited[vertex]) {
+            return;
+        }
+
+        visited[vertex] = true;
+
+        for (int u : graph.adjacentVerticesTo(vertex)) {
+            if (!visited[u]) {
+                treeEdges.add(new UndirectedEdge(vertex, u, EdgeType.TREE_EDGE));
+                dfs(u, vertex);
+            } else if (u != parent) {
+                backEdges.add(new UndirectedEdge(vertex, u, EdgeType.BACK_EDGE));
+            }
+        }
     }
 
     public Collection<UndirectedEdge> getTreeEdges() {
-        throw new UnsupportedOperationException("Implement me!");
+        return Collections.unmodifiableCollection(treeEdges);
     }
 
     public Collection<UndirectedEdge> getBackEdges() {
-        throw new UnsupportedOperationException("Implement me!");
+        return Collections.unmodifiableCollection(backEdges);
     }
 
 }
+
