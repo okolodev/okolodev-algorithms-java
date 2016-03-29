5d9d4e7944e5e8f4b52413a776f317988943aa8a
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2016-03-29 21:16:11 +0200

    Connected Components and Strongly Connected Components 27-th meetup.

--- src/main/java/meetup_27_connected_components_and_strong_components/ConnectedComponents.java
+++ src/main/java/meetup_27_connected_components_and_strong_components/ConnectedComponents.java
@@ -2,6 +2,7 @@ package meetup_27_connected_components_and_strong_components;
 
 import meetup_23_celebrity_problem_and_undirected_graph_representation.UndirectedGraph;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,17 +14,49 @@ import java.util.List;
  * @see <a href="https://en.wikipedia.org/wiki/Connected_component_(graph_theory)">Connected component (graph_theory)</a>
  */
 public class ConnectedComponents {
+    private final UndirectedGraph graph;
+    private final boolean[] visited;
+    private final List<Collection<Integer>> components;
 
     public ConnectedComponents(UndirectedGraph graph) {
-        throw new UnsupportedOperationException("Implement me!");
+        this.graph = graph;
+        visited = new boolean[graph.vertices()];
+        components = new ArrayList<>();
+
+        dfs();
+    }
+
+    private void dfs() {
+        for (int startVertex = 0; startVertex < graph.vertices(); startVertex++) {
+            if (!visited[startVertex]) {
+                components.add(new ArrayList<>());
+                dfs(startVertex);
+            }
+        }
+    }
+
+    private void dfs(int vertex) {
+        if (visited[vertex]) {
+            return;
+        }
+
+        visited[vertex] = true;
+        Collection<Integer> currentComponent =
+                components.get(components.size() - 1);
+        currentComponent.add(vertex);
+
+        for (int u : graph.adjacentVerticesTo(vertex)) {
+            dfs(u);
+        }
     }
 
     public int componentNumber() {
-        throw new UnsupportedOperationException("Implement me!");
+        return components.size();
     }
 
     public List<Collection<Integer>> components() {
-        throw new UnsupportedOperationException("Implement me!");
+        // WARNING: Leaking encapsulation.
+        return components;
     }
 
 }
--- src/main/java/meetup_27_connected_components_and_strong_components/StronglyConnectedComponents.java
+++ src/main/java/meetup_27_connected_components_and_strong_components/StronglyConnectedComponents.java
@@ -1,7 +1,10 @@
 package meetup_27_connected_components_and_strong_components;
 
 import meetup_22_graph_representation.DirectedGraph;
+import stack1.adt.LinkedStack;
+import stack1.adt.Stack;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,17 +16,79 @@ import java.util.List;
  * @see <a href="https://en.wikipedia.org/wiki/Strongly_connected_component">Strongly connected component</a>
  */
 public class StronglyConnectedComponents {
+    private final DirectedGraph digraph;
+    private final boolean[] visited;
+    private final List<Collection<Integer>> strongComponents;
+
+    private final boolean[] postOrderVisited;
 
     public StronglyConnectedComponents(DirectedGraph digraph) {
-        throw new UnsupportedOperationException("Implement me!");
+        this.digraph = digraph;
+        visited = new boolean[digraph.vertices()];
+        strongComponents = new ArrayList<>();
+
+        postOrderVisited = new boolean[digraph.vertices()];
+
+        Stack<Integer> postOrder = dfsPostOrder(digraph.reverse());
+
+        while (!postOrder.isEmpty()) {
+            int startVertex = postOrder.pop();
+
+            if (!visited[startVertex]) {
+                strongComponents.add(new ArrayList<>());
+                dfs(startVertex);
+            }
+        }
+    }
+
+    private void dfs(int vertex) {
+        if (visited[vertex]) {
+            return;
+        }
+
+        visited[vertex] = true;
+        Collection<Integer> currentComponent =
+                strongComponents.get(strongComponents.size() - 1);
+        currentComponent.add(vertex);
+
+        for (int u : digraph.adjacentVerticesTo(vertex)) {
+            dfs(u);
+        }
+    }
+
+    private Stack<Integer> dfsPostOrder(DirectedGraph reversed) {
+        Stack<Integer> postOrder = new LinkedStack<>();
+
+        for (int startVertex = 0; startVertex < reversed.vertices(); startVertex++) {
+            dfsPostOrder(reversed, startVertex, postOrder);
+        }
+
+        return postOrder;
+    }
+
+    private void dfsPostOrder(DirectedGraph reversed,
+                              int vertex,
+                              Stack<Integer> postOrder) {
+        if (postOrderVisited[vertex]) {
+            return;
+        }
+
+        postOrderVisited[vertex] = true;
+
+        for (int u : reversed.adjacentVerticesTo(vertex)) {
+            dfsPostOrder(reversed, u, postOrder);
+        }
+
+        postOrder.push(vertex);
     }
 
     public int componentNumber() {
-        throw new UnsupportedOperationException("Implement me!");
+        return strongComponents.size();
     }
 
     public List<Collection<Integer>> components() {
-        throw new UnsupportedOperationException("Implement me!");
+        // WARNING: Leaking encapsulation
+        return strongComponents;
     }
 
 }
