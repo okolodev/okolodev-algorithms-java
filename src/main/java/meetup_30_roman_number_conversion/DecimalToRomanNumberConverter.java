f33b0caf0482d24122164c4081e43c8f73ae88d2
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2016-05-10 19:12:16 +0200

    Failing tests for the Roman to Decimal and back Conversion for the 30-th meetup.

---
+++ src/main/java/meetup_30_roman_number_conversion/DecimalToRomanNumberConverter.java
@@ -0,0 +1,9 @@
+package meetup_30_roman_number_conversion;
+
+public class DecimalToRomanNumberConverter {
+
+    public String convertDecimalToRoman(int decimal) {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+
+}
---
+++ src/main/java/meetup_30_roman_number_conversion/RomanToDecimalNumberConverter.java
@@ -0,0 +1,9 @@
+package meetup_30_roman_number_conversion;
+
+public class RomanToDecimalNumberConverter {
+
+    public int convertRomanToDecimal(String roman) {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+
+}
