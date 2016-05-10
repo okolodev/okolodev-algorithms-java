da6a07e8014e9f9d1e0097cd8ece5583b2898a0e
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2016-05-10 21:20:27 +0200

    Roman to Decimal and back Conversion for the 30-th meetup.

--- src/main/java/meetup_30_roman_number_conversion/DecimalToRomanNumberConverter.java
+++ src/main/java/meetup_30_roman_number_conversion/DecimalToRomanNumberConverter.java
@@ -1,9 +1,39 @@
 package meetup_30_roman_number_conversion;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class DecimalToRomanNumberConverter {
+    private Map<Integer, String> decimalToRoman = new LinkedHashMap<>();
+
+    public DecimalToRomanNumberConverter() {
+        decimalToRoman.put(1000, "M");
+        decimalToRoman.put(900, "CM");
+        decimalToRoman.put(500, "D");
+        decimalToRoman.put(400, "CD");
+        decimalToRoman.put(100, "C");
+        decimalToRoman.put(90, "XC");
+        decimalToRoman.put(50, "L");
+        decimalToRoman.put(40, "XL");
+        decimalToRoman.put(10, "X");
+        decimalToRoman.put(9, "IX");
+        decimalToRoman.put(5, "V");
+        decimalToRoman.put(4, "IV");
+        decimalToRoman.put(1, "I");
+    }
 
     public String convertDecimalToRoman(int decimal) {
-        throw new UnsupportedOperationException("Implement me!");
+        StringBuilder roman = new StringBuilder();
+
+        for (int base : decimalToRoman.keySet()) {
+            int times = decimal / base;
+            for (int i = 0; i < times; i++) {
+                roman.append(decimalToRoman.get(base));
+            }
+            decimal = decimal % base;
+        }
+
+        return roman.toString();
     }
 
 }
--- src/main/java/meetup_30_roman_number_conversion/RomanToDecimalNumberConverter.java
+++ src/main/java/meetup_30_roman_number_conversion/RomanToDecimalNumberConverter.java
@@ -1,9 +1,69 @@
 package meetup_30_roman_number_conversion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToDecimalNumberConverter {
+    private Map<Character, Integer> romanToDecimal = new HashMap<>();
+
+    public RomanToDecimalNumberConverter() {
+        romanToDecimal.put('I', 1);
+        romanToDecimal.put('V', 5);
+        romanToDecimal.put('X', 10);
+        romanToDecimal.put('L', 50);
+        romanToDecimal.put('C', 100);
+        romanToDecimal.put('D', 500);
+        romanToDecimal.put('M', 1000);
+    }
 
     public int convertRomanToDecimal(String roman) {
-        throw new UnsupportedOperationException("Implement me!");
+        int decimal = 0;
+        int previousNumber = 0;
+
+        // IX
+        // XI
+        char[] chars = roman.toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            int currentNumber = romanToDecimal.get(chars[i]);
+
+            if (currentNumber < previousNumber) {
+                decimal -= currentNumber;
+            } else {
+                decimal += currentNumber;
+            }
+
+            previousNumber = currentNumber;
+        }
+
+        return decimal;
+    }
+
+    public int convertRomanToDecimal2(String roman) {
+        int decimal = 0;
+        int localDecimal = 0;
+
+        for (int i = 0; i < roman.length(); i++) {
+            char symbol = roman.charAt(i);
+            char previousSymbol = 'I';
+            if (i > 0) {
+                previousSymbol = roman.charAt(i - 1);
+            }
+
+            // IX ->
+            // XI ->
+            Integer current = romanToDecimal.get(symbol);
+            Integer previous = romanToDecimal.get(previousSymbol);
+
+            if (current > previous) {
+                decimal -= localDecimal;
+            } else {
+                decimal += localDecimal;
+            }
+
+            localDecimal = current;
+        }
+
+        return decimal + localDecimal;
     }
 
 }
