29aa4d332db3f84b3d36056e1144c75980de2f85
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2016-05-17 22:06:13 +0200

    The Bloom Filter for the 31-st meetup.

--- src/main/java/meetup_31_bloom_filter/BloomFilter.java
+++ src/main/java/meetup_31_bloom_filter/BloomFilter.java
@@ -1,25 +1,72 @@
 package meetup_31_bloom_filter;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.BitSet;
+
 public class BloomFilter {
+    private final int size;
+    private final int hashFunctionNumber;
+    private final BitSet filter;
+
+    private MessageDigest md5Function;
+    private MessageDigest sha1Function;
 
     public BloomFilter(int size, int hashFunctionNumber) {
-        throw new UnsupportedOperationException("Implement me!");
+        this.size = size;
+        this.hashFunctionNumber = hashFunctionNumber;
+        filter = new BitSet(size);
+
+        try {
+            md5Function = MessageDigest.getInstance("MD5");
+            sha1Function = MessageDigest.getInstance("SHA1");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getSize() {
-        throw new UnsupportedOperationException("Implement me!");
+        return size;
     }
 
     public int getHashFunctionNumber() {
-        throw new UnsupportedOperationException("Implement me!");
+        return hashFunctionNumber;
     }
 
     public void put(String key) {
-        throw new UnsupportedOperationException("Implement me!");
+        int md5Hash = computeHash(md5Function, key);
+        int sha1Hash = computeHash(sha1Function, key);
+
+        for (int i = 1; i <= hashFunctionNumber; i++) {
+            int doubleHash = md5Hash + i * sha1Hash;
+            int bitIndex = Math.abs(doubleHash) % size;
+            filter.set(bitIndex, true);
+        }
     }
 
     public boolean contains(String key) {
-        throw new UnsupportedOperationException("Implement me!");
+        int md5Hash = computeHash(md5Function, key);
+        int sha1Hash = computeHash(sha1Function, key);
+
+        for (int i = 1; i <= hashFunctionNumber; i++) {
+            int doubleHash = md5Hash + i * sha1Hash;
+            int bitIndex = Math.abs(doubleHash) % size;
+
+            if (!filter.get(bitIndex)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int computeHash(MessageDigest hashFunction, String key) {
+        hashFunction.reset();
+        hashFunction.update(key.getBytes());
+        byte[] digest = hashFunction.digest();
+
+        return new BigInteger(1, digest).intValue();
     }
 
 }
