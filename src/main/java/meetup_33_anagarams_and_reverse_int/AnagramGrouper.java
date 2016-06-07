edf48e1b83024b5c024f4e519e1e47a0901ca1f3
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2016-06-07 23:40:35 +0200

    Anagram Groups and Reverse Integer 33-rd meetup.

--- src/main/java/meetup_33_anagarams_and_reverse_int/AnagramGrouper.java
+++ src/main/java/meetup_33_anagarams_and_reverse_int/AnagramGrouper.java
@@ -1,6 +1,10 @@
 package meetup_33_anagarams_and_reverse_int;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 49. Group Anagrams
@@ -10,7 +14,27 @@ import java.util.List;
 public class AnagramGrouper {
 
     public List<List<String>> groupAnagrams(String... words) {
-        throw new UnsupportedOperationException("Implement me!");
+        Arrays.sort(words);
+
+        Map<String, List<String>> map = new LinkedHashMap<>();
+        for (String word : words) {
+            String wordWithSortedLetters = sortWordLetters(word);
+
+            if (!map.containsKey(wordWithSortedLetters)) {
+                map.put(wordWithSortedLetters, new ArrayList<>());
+            }
+
+            map.get(wordWithSortedLetters).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    private String sortWordLetters(String word) {
+        char[] letters = word.toCharArray();
+        Arrays.sort(letters);
+
+        return new String(letters);
     }
 
 }
--- src/main/java/meetup_33_anagarams_and_reverse_int/ReverseInteger.java
+++ src/main/java/meetup_33_anagarams_and_reverse_int/ReverseInteger.java
@@ -8,11 +8,50 @@ package meetup_33_anagarams_and_reverse_int;
 public class ReverseInteger {
 
     public int reverse(int original) {
-        throw new UnsupportedOperationException("Implement me!");
+        boolean positive = original > 0;
+        int absolute = Math.abs(original);
+        int reversed = 0;
+
+        while (absolute > 0) {
+            int lastDigit = absolute % 10;
+            int newReversed = reversed * 10 + lastDigit;
+
+            // overflow check
+            if ((newReversed - lastDigit) / 10 != reversed) {
+                return 0;
+            }
+
+            reversed = newReversed;
+            absolute = absolute / 10;
+        }
+
+        if (!positive) {
+            return -reversed;
+        }
+
+        return reversed;
     }
 
     public int reverse2(int original) {
-        throw new UnsupportedOperationException("Implement me!");
+        boolean positive = original > 0;
+        int absolute = Math.abs(original);
+        long reversed = 0;
+
+        while (absolute > 0) {
+            int lastDigit = absolute % 10;
+            reversed = reversed * 10 + lastDigit;
+            absolute = absolute / 10;
+        }
+
+        if (reversed > Integer.MAX_VALUE) {
+            return 0;
+        }
+
+        if (!positive) {
+            return (int) -reversed;
+        }
+
+        return (int) reversed;
     }
 
 }
